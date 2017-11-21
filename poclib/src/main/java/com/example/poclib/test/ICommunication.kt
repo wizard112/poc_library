e0f4680e9b4cd32d28a8f66340ec4697bb4a9be5
Malcom <gaeltshilombo@gmail.com>
2017-11-21 11:41:55 +0100

    add interface to communicate between application and the library

---
+++ poclib/src/main/java/com/example/poclib/test/ICommunication.kt
@@ -0,0 +1,13 @@
+package com.example.poclib.test
+
+import android.content.Intent
+
+/**
+ * Created by gael on 21.11.17.
+ */
+
+interface ICommunication {
+
+    fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray)
+    fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?)
+}
