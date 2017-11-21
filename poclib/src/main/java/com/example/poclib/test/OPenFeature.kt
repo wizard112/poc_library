78be0968fb30c8cdf07a403840ff95927998bc15
Malcom <gaeltshilombo@gmail.com>
2017-11-21 11:40:54 +0100

    add class to open implicit intent

---
+++ poclib/src/main/java/com/example/poclib/test/OPenFeature.kt
@@ -0,0 +1,17 @@
+package com.example.poclib.test
+
+import android.content.Context
+import android.content.Intent
+import android.provider.MediaStore
+import android.support.v7.app.AppCompatActivity
+
+/**
+ * Created by gael on 21.11.17.
+ */
+
+object OPenFeature {
+
+    fun newIntentCamera(activity: AppCompatActivity){
+        activity.startActivityForResult(Intent(MediaStore.ACTION_IMAGE_CAPTURE),PermissionConstants.CODE_PERMISSION_CAMERA)
+    }
+}
