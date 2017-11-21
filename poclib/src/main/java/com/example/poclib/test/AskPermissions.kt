7749f82719d892e0a6774cd3c928c74e4a520f79
Malcom <gaeltshilombo@gmail.com>
2017-11-21 11:42:14 +0100

    add class to ask permissions

---
+++ poclib/src/main/java/com/example/poclib/test/AskPermissions.kt
@@ -0,0 +1,31 @@
+package com.example.poclib.test
+
+import android.Manifest
+import android.content.Context
+import android.content.pm.PackageManager
+import android.support.v4.app.ActivityCompat
+import android.support.v7.app.AppCompatActivity
+
+/**
+ * Created by gael on 21.11.17.
+ */
+
+object AskPermissions {
+
+    private var manager : ManageResult? = null
+
+    fun getInstance(context: Context, activity: AppCompatActivity) : ManageResult{
+        if(manager == null) {
+            manager = ManageResult(context,activity)
+        }
+        return manager!!
+    }
+
+    fun checkPermissionCamera(activity: AppCompatActivity) {
+        if(ActivityCompat.checkSelfPermission(activity,Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(activity, arrayOf(Manifest.permission.CAMERA),PermissionConstants.CODE_PERMISSION_CAMERA)
+        }else{
+            OPenFeature.newIntentCamera(activity)
+        }
+    }
+}
