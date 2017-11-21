d1256de6091407bd690c8bf55bed7bc843ab6696
Malcom <gaeltshilombo@gmail.com>
2017-11-21 11:44:34 +0100

    convert file to kotlin language

---
+++ app/src/main/java/com/example/gael/poclibtest/MainActivity.kt
@@ -0,0 +1,49 @@
+package com.example.gael.poclibtest
+
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import com.example.poclib.test.AskPermissions
+import com.example.poclib.test.ManageResult
+import com.example.poclib.test.OPenFeature
+import com.example.poclib.test.PermissionConstants
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity(), View.OnClickListener{
+
+    var manager : ManageResult? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        manager = ManageResult(this,this)
+
+        btn_camera_permission.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+       when(v!!.id) {
+           R.id.btn_camera_permission -> {
+               AskPermissions.checkPermissionCamera(this)
+           }
+       }
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        //AskPermissions.getInstance(this,this).onRequestPermissionsResult(requestCode, permissions, grantResults)
+        manager!!.onRequestPermissionsResult(requestCode, permissions, grantResults)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        //AskPermissions.getInstance(this,this).onActivityResult(requestCode, resultCode, data)
+        manager!!.onActivityResult(requestCode, resultCode, data)
+    }
+
+}
