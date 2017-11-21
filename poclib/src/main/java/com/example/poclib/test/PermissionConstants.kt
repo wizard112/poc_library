b6ad2b261cea62744e15b12d9fe482c86daaffa7
Malcom <gaeltshilombo@gmail.com>
2017-11-21 11:40:27 +0100

    add constants for permissions

---
+++ poclib/src/main/java/com/example/poclib/test/PermissionConstants.kt
@@ -0,0 +1,11 @@
+package com.example.poclib.test
+
+/**
+ * Created by gael on 21.11.17.
+ */
+
+object PermissionConstants {
+
+    val ASK_PERMISSION_CAMERA = 1
+    val CODE_PERMISSION_CAMERA = 500
+}
